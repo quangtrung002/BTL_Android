ac4cdd546fea66fe7ed8539996c6d9b8bd84e2df
quangtrung <trungbina3qo@gmail.com>
2023-03-31 16:31:27 +0700

    setup project: create navigation

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.musicplayer.databinding.ActivityMainBinding;
+import com.example.musicplayer.ui.AlbumFragment;
+import com.example.musicplayer.ui.HomeFragment;
+import com.example.musicplayer.ui.ListFragment;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.album:
+                    replaceFragment(new AlbumFragment());
+                    break;
+                case R.id.list:
+                    replaceFragment(new ListFragment());
+                    break;
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
