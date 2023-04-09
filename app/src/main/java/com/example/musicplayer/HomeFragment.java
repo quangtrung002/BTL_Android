059a54ce4cebb51054afa097c8732b4da431eff2
trungbin002 <trungbina3qo@gmail.com>
2023-04-09 15:15:06 +0700

    create top music in home page

--- app/src/main/java/com/example/musicplayer/HomeFragment.java
+++ app/src/main/java/com/example/musicplayer/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.musicplayer;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,37 +16,23 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 
+import java.io.File;
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+    RecyclerView recyclerView;
+    ArrayList<AudioModel> songsList = new ArrayList<>();
 
+    public HomeFragment() {
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -65,9 +55,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view);
+
         ImageSlider imageSlider = view.findViewById(R.id.imageSlider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
-
 
         slideModels.add(new SlideModel(R.drawable.slide1, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.slide2, ScaleTypes.FIT));
@@ -75,6 +66,34 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.slide4, ScaleTypes.FIT));
 
         imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+
+
+        String[] projection = {
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.ARTIST
+        };
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+
+        Cursor cursor = getActivity().getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null, null);
+
+        while (cursor.moveToNext()) {
+            String path = cursor.getString(0);
+            String title = cursor.getString(1);
+            String duration = cursor.getString(2);
+            String artist = cursor.getString(3);
+
+            AudioModel songData = new AudioModel(path, title, duration, artist);
+            if (new File(songData.getPath()).exists())
+                songsList.add(songData);
+        }
+
+        ArrayList<AudioModel> songsTop = new ArrayList<>(songsList.subList(3,8));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new MusicListAdapter(songsTop, getActivity().getApplicationContext()));
 
 
         return view;
